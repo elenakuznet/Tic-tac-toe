65cd3217c2ae3f750c514a77573cde2398c3c15f
Elena Kuznetsova <kuzn.elena@yahoo.com>
2023-09-02 23:33:19 +0500

    create game board

---
+++ index.js
@@ -0,0 +1,18 @@
+const gameBoard = document.querySelector('#gameboard');
+const infoDisplay = document.querySelector('#info');
+
+const startCells = [
+    '', '', '', '', '', '', '', '', '',
+];
+
+function createBoard () {
+    startCells.forEach((cell, index) => {
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('square');
+        gameBoard.append(cellElement);
+    })
+}
+
+createBoard();
+
+
